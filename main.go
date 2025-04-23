e3b7c78609041d54d13e48e2fcf1339c494640ee
gayabarumalamselatan <soerjahartono22@gmail.com>
2025-04-24 01:16:18 +0700

    fix main.go

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import (
 	"auth-service/routes"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
+
+// func Handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+// }
+
 
 func init() {
 	 config.DatabaseCon()
@@ -23,9 +27,12 @@ func main() {
     log.Fatal("Error loading .env file")
   }
 
-  envtest := os.Getenv("PORT")
+  port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-	fmt.Println("Hello", envtest)
+	fmt.Println("Hello", port)
 
 	r := gin.Default()
 
@@ -48,11 +55,10 @@ func main() {
 	routes.TokenRoutes(tokenRoutes)
 	roleRoutes := r.Group("/role-service")
 	routes.RoleRoutes(roleRoutes)
-	r.Run()
-}
-
-func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	// r.Run()
+	if err := r.Run(":" + port); err != nil {
+		log.Fatal("Failed to start server:", err)
+	}
 }
 
 // CompileDaemon -command="./dummyservice"
