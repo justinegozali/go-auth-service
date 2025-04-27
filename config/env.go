e5bc4480a34e1b9ea3b41d7c6b4272defac5ffc8
gayabarumalamselatan <soerjahartono22@gmail.com>
2025-04-28 00:04:45 +0700

    tututu

--- config/env.go
+++ config/env.go
@@ -2,13 +2,27 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func EnvInit() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading env file")
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading env file")
+	// }
+
+	if os.Getenv("VERCEL") == "" {
+		// Running locally → load .env file
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Warning: Error loading .env file, proceeding with system environment variables")
+		} else {
+			log.Println(".env file loaded successfully")
+		}
+	} else {
+		// Running on Vercel → DO NOT load .env
+		log.Println("Running on Vercel, using system environment variables")
 	}
 }
